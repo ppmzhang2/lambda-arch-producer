f6ec131d789556e3b790c5de225e858df14718cc
Zhang <ztz2000@gmail.com>
2020-08-24 22:36:44 +0800

    [fix] re-org

--- app/trades_producer.py
+++ app/trades_producer.py
@@ -13,22 +13,20 @@ from config import Config
 
 
 class TradesProducer(object):
-    __slots__ = ['_topic', '_symbols', '_finn_ws', '_finn_producer']
+    __slots__ = ['_topic', '_finn_ws', '_finn_producer']
 
-    def __init__(self, *symbols: str):
+    def __init__(self):
         self._topic = Config.KAFKA_TOPIC
-        self._symbols = symbols
         self._finn_ws = FinnHubWs()
         self._finn_producer = FinnHubProducer()
 
-    async def send_trades(self):
+    async def send_trades(self, *symbols: str):
         async def cb(dc: dict):
-            print(dc)
             b = json.dumps(dc).encode('utf8')
             await self._finn_producer.send(self._topic, b)
 
-        await self._finn_ws.trades_stream(cb, *self._symbols)
+        await self._finn_ws.trades_stream(cb, *symbols)
 
-    def run(self) -> NoReturn:
+    def run(self, *symbols: str) -> NoReturn:
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.send_trades())
+        loop.run_until_complete(self.send_trades(*symbols))
